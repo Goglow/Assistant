1ce08352d521bc3eeb8ea381f01d3c4c655a9f1b
Goglow <waggle.glow@gmail.com>
2019-11-08 14:14:39 -0400

    Update different files

--- Assistant/Assistant/Controller/ViewController.swift
+++ Assistant/Assistant/Controller/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
     class ViewController: UIViewController {
         
+        @IBOutlet weak var assistantLabel: UILabel!
+        @IBOutlet weak var deleteButton: UIButton!
+        @IBOutlet weak var addButton: UIButton!
+        
         override func viewDidLoad() {
             super.viewDidLoad()
 
